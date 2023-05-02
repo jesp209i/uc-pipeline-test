ff4d9d1f5f241dcb29b2bc1da424f0800360f552
Jesper Madsen <jam@umbraco.dk>
2023-05-02 12:38:18 +0200

    scripts and workflow

---
+++ scripts/upload_package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set variables
+url="https://apim-dev-global.azure-api.net/global/project/cicd-prototype/api/public/projects/$1/deployments/$2/package"
+token="$3"
+file="$4"
+
+function call_api {
+  response=$(curl -s -X POST $url \
+    -H "Ocp-Apim-Subscription-Key: $token" \
+    -H "Content-Type: multipart/form-data" \
+    --form "file=@$file")
+
+  echo "$response"
+
+  # TODO: error handling not 202 response here
+#  http_code=$(echo "$response" | awk '/HTTP/{print $2}')
+#  if [[ $http_code != "202" ]]; then
+#    echo "Unexpected HTTP response code: $http_code"
+#    exit 1
+#  fi
+}
+
+call_api
