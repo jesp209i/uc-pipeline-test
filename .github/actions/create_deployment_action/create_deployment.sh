b20307784ded78ba25e3d8b3d859eee6522aa1c4
Jesper Madsen <jam@umbraco.dk>
2023-05-02 19:28:29 +0200

    Add Action

--- .github/actions/create_deployment_action/create_deployment.sh
+++ .github/actions/create_deployment_action/create_deployment.sh
@@ -12,23 +12,17 @@ function call_api {
     -H "Ocp-Apim-Subscription-Key: $token" \
     -H "Content-Type: application/json" \
     -d "{\"commitMessage\":\"$commitMessage\"}")
-  echo "$response"
-#  http_code=$(echo "$response" | awk '/HTTP/{print $2}')
-#  if [[ $http_code != "201" ]]; then
-#    echo "Unexpected HTTP response code: $http_code"
-#    exit 1
-#  fi
+
   status=$(echo "$response" | jq -r '.deploymentState')
   deployment_id=$(echo "$response" | jq -r '.deploymentId')
   if [[ $status != "Pending" ]]; then
     echo "Unexpected status: $status"
     exit 1
   fi
-  echo "$deployment_id"
+
+  echo "Deployment started successfully -> $deployment_id"
 }
 
 call_api
 
-echo "Deployment started successfully -> $deployment_id"
-
-echo "DEPLOYMENT_ID=$deployment_id" >> "$GITHUB_OUTPUT"
+echo "DEPLOYMENT_ID=$deployment_id" >> $GITHUB_OUTPUT
