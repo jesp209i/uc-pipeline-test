c148e4cdaf6b526e196cb1be2b2eb0ac4c263762
Jesper Madsen <jam@umbraco.dk>
2023-05-03 13:09:28 +0200

    move files

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { resolve } from 'path'
 
 export default defineConfig({
   build: {
-    outDir: '../UmbracoProject/wwwroot/app/', // inside the Umbraco project
+    outDir: 'src/UmbracoProject/wwwroot/app/', // inside the Umbraco project
     emptyOutDir: true, // needs to be explicitly set because it’s outside of ./
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/main.ts')
+        main: resolve(__dirname, 'src/frontend/src/main.ts')
       }
     }
   }
