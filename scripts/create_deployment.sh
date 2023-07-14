2b7ca359e19c0413e7246aef2ca4d5f3d7702527
Jesper Madsen <jam@umbraco.dk>
2023-07-14 11:29:07 +0200

    script fix

--- scripts/create_deployment.sh
+++ scripts/create_deployment.sh
@@ -14,14 +14,10 @@ function call_api {
     -H "Content-Type: application/json" \
     -d "{\"commitMessage\":\"$commitMessage\"}")
   echo "$response"
-#  http_code=$(echo "$response" | awk '/HTTP/{print $2}')
-#  if [[ $http_code != "201" ]]; then
-#    echo "Unexpected HTTP response code: $http_code"
-#    exit 1
-#  fi
+
   status=$(echo "$response" | jq -r '.deploymentState')
   deployment_id=$(echo "$response" | jq -r '.deploymentId')
-  if [[ $status != "Pending" ]]; then
+  if [[ $status != "Created" ]]; then
     echo "Unexpected status: $status"
     exit 1
   fi
