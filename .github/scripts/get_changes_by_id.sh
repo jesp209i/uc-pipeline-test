7f9a91a3051b1516f82ebe19cbbdda615a189b10
Jesper Madsen <jesper.a.madsen@gmail.com>
2024-06-28 10:19:51 +0200

    get changes show errors?

--- .github/scripts/get_changes_by_id.sh
+++ .github/scripts/get_changes_by_id.sh
@@ -46,7 +46,7 @@ function get_changes {
     return
   fi
   echo "---Response Start---"
-  echo $Response
+  cat $filePath
   echo "---Response End---"
   echo "Unexpected response - see above"
   exit 1
